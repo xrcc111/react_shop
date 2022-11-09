25515ab3a4bec17a106794d095691883ffedfce4
徐锐 <2603029264@qq.com>
2022-11-10 01:20:57 +0800

    递归菜单实现

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function Navbar(props) {
+  const { menuArray } = props
+  // 控制字迹展开
+  const [active, setActive] = useState(false)
+  // 递归菜单
+  const recursionMenus = (menuArray.children || []).map((el, i) => {
+    return (
+      <Navbar key = {i}  menuArray = { el }></Navbar>
+    )
+  })
+
+  // 点击事件
+  const handelClick = (i) => {
+    if(i.children && i.children.length > 0) {
+      setActive(true)
+    }else{
+      console.log('======')
+    }
+  }
+  return (
+    <div className='nav'>
+      <span onClick={() => handelClick(menuArray)}>{menuArray.catalogName}</span>
+      <div style={{marginLeft: '20px', display: active ? 'block' : 'none'}}>{ recursionMenus }</div>
+    </div>
+  )
+}
--- src/layouts/components/Menu/index.jsx
+++ src/layouts/components/Menu/index.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux/es/exports'
 import { loadMenu } from '@/store/modules/menu'
+import Navbar from '@/components/Navbar'
 import "./index.less"
 
 export default function Menu() {
   const dispatch = useDispatch()
   const { menuArray, open } = useSelector(state => state.menu)
-  console.log(open)
+  console.log(menuArray)
   
   useEffect(() => {
     dispatch(loadMenu({}))
   },[])
   return (
-    <div className='top-navigation' style={{transform: open ? 'translateY(0)'  : 'translateY(-118%)'}}>{menuArray.map(i => <li key={i.catalogId}>{i.catalogName}</li>)}</div>
+    <div className='top-navigation' style={{transform: open ? 'translateY(0)'  : 'translateY(-118%)'}}>
+      {
+        menuArray.map(i => 
+          <Navbar key={ i.catalogId } menuArray = { i || []}> 
+          </Navbar>)
+      }
+      {/* <Navbar menuArray = {menuArray}></Navbar> */}
+    </div>
   )
 }
